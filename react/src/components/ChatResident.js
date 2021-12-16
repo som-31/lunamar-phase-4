c22d369f30b97a6f9796d866fdc3b46fdb752b9a
Rohit padwal <rpadwal98@gmail.com>
2021-12-15 18:10:30 -0600

    Building crud

--- react/src/components/ChatResident.js
+++ react/src/components/ChatResident.js
@@ -1,50 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-function ChatResident(){
-   
+function Resident() {
 
-    
-    return(
+    return (
         <>
-   
 
-   <Link to='/manage-service-resident'>Manage Service</Link>
-                    <Link to='/manage-visitor-resident'>Manage Visitor</Link>
-                    <Link to='/photo-video'>Upload Photo/video</Link>
+            <div class="sidebar">
 
-                    <Link to='/chat-resident'>Chat</Link>
-                        <div>
-                        <h1 style={{textAlign:"center"}}>
-            Welcome, Visitor
-        </h1>
+                <Link to='/manage-service-resident'>Manage Service</Link>
+                <Link to='/manage-visitor-resident'>Manage Visitor</Link>
+                <Link to='/photo-video'>Upload Photo/video</Link>
+                <Link to='/manage-incident'>Manage Incident</Link>
 
-        <button class="open-button" onClick={openForm}>Chat</button>
+                <Link to='/chat-resident'>Chat</Link>
+            </div>
+            <div>
+                <h1 style={{ "text-align": "center" }}>
+                    Welcome
+                </h1>
+            </div>
 
-        <div class="chat-popup" id="myForm">
-          <form  class="form-container">
-            <h1>Chat</h1>
-        
-            <label for="msg"><b>Message</b></label>
-            <textarea placeholder="Type message.." name="msg" required></textarea>
-        
-            <button type="submit" class="btn">Send</button>
-            <button type="button" class="btn cancel" onClick={closeForm}>Close</button>
-          </form>
-        </div>
-        
-       
-    </div>
-        
 
-      </>
+
+        </>
     );
 }
 
-export default ChatResident;
+export default Resident;
